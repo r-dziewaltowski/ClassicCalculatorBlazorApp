91f37284473e84de498d279b88984094f9dd9730
r-dziewaltowski <135457365+r-dziewaltowski@users.noreply.github.com>
2025-03-28 13:51:14 +0000

    Logging sorted out

--- src/ClassicCalculatorBlazorApp/Program.cs
+++ src/ClassicCalculatorBlazorApp/Program.cs
@@ -4,21 +4,26 @@ using ClassicCalculatorBlazorApp;
 using ClassicCalculator;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped<ICalculator, Calculator>(provider => new Calculator(displayLength: 14));
-
-// Configure logging
-builder.Logging.SetMinimumLevel(LogLevel.Information);
+builder.Services.AddScoped<ICalculator>(sp =>
+{
+    var logger = sp.GetRequiredService<ILogger<Calculator>>();
+    return new Calculator(displayLength: 14, logger);
+});
 
 var host = builder.Build();
+
+var logger = host.Services.GetRequiredService<ILogger<Program>>();
+logger.LogInformation("App starting...");
 
 // Global exception handling
 AppDomain.CurrentDomain.UnhandledException += (sender, eventArgs) =>
 {
     var exception = eventArgs.ExceptionObject as Exception;
-    var logger = host.Services.GetRequiredService<ILogger<Program>>();
     logger.LogError(exception, "Unhandled exception occurred.");
 };
 
